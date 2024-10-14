ee80c1c160d1801fe3fe506bc44dcb0d57be2eaf
Dylan De La Rosa <delarosadyl@gmail.com>
2024-10-14 16:06:48 -0500

    adding in formats and pngs for all viewports

--- resume-website/src/app/page.tsx
+++ resume-website/src/app/page.tsx
@@ -11,7 +11,21 @@ export default function Home() {
         name="description"
         content="Resume website by Dylan De La Rosa"></meta>
       <link rel="icon" href="/favicon.ico" />
-
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"></link>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"></link>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
       <div className="App">
         <Resume />
       </div>
