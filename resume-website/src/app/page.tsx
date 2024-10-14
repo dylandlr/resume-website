a961a30b432a9aa996328e690aeb8c584a455e80
Dylan De La Rosa <delarosadyl@gmail.com>
2024-10-14 16:00:49 -0500

    refining the site

--- resume-website/src/app/page.tsx
+++ resume-website/src/app/page.tsx
@@ -2,9 +2,16 @@ import { Resume } from "@/components/resume";
 export default function Home() {
   return (
     <>
+      <meta charSet="utf-8" />
       <title>Dylan M. De La Rosa</title>
+      <meta name="author" content="Dylan M. De La Rosa" />
+      <meta name="robots" content="index, follow" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Resume website by Dylan De La Rosa"></meta>
       <link rel="icon" href="/favicon.ico" />
+
       <div className="App">
         <Resume />
       </div>
